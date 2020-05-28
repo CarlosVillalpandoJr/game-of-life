b0ffa9da56eb1aa68548d88a9566c419fb985a8c
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-05-28 11:46:16 -0500

    mapping through grid and displaying boxes for each element

--- game/src/App.js
+++ game/src/App.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+const numberRows = 40
+const numberCols = 40
 
 function App() {
+  // useState will only run once with function
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numberRows; i++) {
+      // pushing column to rows array; all values in array init to 0(dead)
+      rows.push(Array.from(Array(numberCols), () => 0))
+    }
+    return rows
+  });
+  
   return (
-    <div>hi</div>
+    <div>
+      {grid.map((rows, i) => rows.map((col, k) => (
+        <div style={{
+          backgroundColor: grid[i][k] ? "blue" : undefined,
+          border: "1px solid black",
+          width: 20,
+          height: 20
+        }}>
+        </div>
+      )))}
+    </div>
   );
 }
 
