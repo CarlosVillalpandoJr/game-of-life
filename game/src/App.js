0af067a7fb44521be4b805a6bedd6c48f0939dce
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-05-28 12:38:37 -0500

    setting up simulation to keep cells changing

--- game/src/App.js
+++ game/src/App.js
@@ -14,34 +14,63 @@ function App() {
     }
     return rows
   });
+
+  const [active, setActive] = useState(false)
+
+  // using useRef to be able to use the current value of active in function
+  const activeRef = useRef(active)
+  activeRef.current = active
+
+  const activeSim = useCallback(() => {
+    if(!active.current) {
+      return;
+    }
+
+    setGrid(g => {
+      return produce(g, gridCopy => {
+        for(let i = 0; i < numberRows; i++) {
+          for(let k = 0; k < numberCols; k++) {
+            let neighbors = 0;
+            
+          }
+        }
+      })
+    })
+
+  })
   
   return (
-    <div style={{
-      display: 'grid',
-      gridTemplateColumns: `repeat(${numberCols}, 20px)`
-    }}
-    >
-      {/* have to map twice because rows is an array */}
-      {grid.map((rows, i) => rows.map((col, k) => (
-        <div 
-        key={`${i}-${k}`}
-        style={{
-          backgroundColor: grid[i][k] ? "blue" : undefined,
-          border: "1px solid black",
-          width: 20,
-          height: 20
-        }}
-        onClick={() => {
-          // Creates an immutable change and generates a new grid
-          const newGrid = produce(grid, gridCopy => {
-            gridCopy[i][k] = grid[i][k] ? 0 : 1
-          });
-          setGrid(newGrid)
-        }}
-        >
-        </div>
-      )))}
-    </div>
+    <>
+      <button onClick={() => {
+        setActive(!active)
+      }}>{active ? 'stop' : 'start'}</button>
+      <div style={{
+        display: 'grid',
+        gridTemplateColumns: `repeat(${numberCols}, 20px)`
+      }}
+      >
+        {/* have to map twice because rows is an array */}
+        {grid.map((rows, i) => rows.map((col, k) => (
+          <div 
+          key={`${i}-${k}`}
+          style={{
+            backgroundColor: grid[i][k] ? "blue" : undefined,
+            border: "1px solid black",
+            width: 20,
+            height: 20
+          }}
+          onClick={() => {
+            // Creates an immutable change and generates a new grid
+            const newGrid = produce(grid, gridCopy => {
+              gridCopy[i][k] = grid[i][k] ? 0 : 1
+            });
+            setGrid(newGrid)
+          }}
+          >
+          </div>
+        )))}
+      </div>
+    </>
   );
 }
 
